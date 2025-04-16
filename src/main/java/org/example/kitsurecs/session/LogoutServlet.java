d28f9be7af53f7da577593eac00e4d5ca21ba589
Derrick Mangari <dmangari@live.fr>
2025-04-15 20:40:58 -0400

    Created Session folder with Session handling login, logout, also created Session manager to get UserId, username, profile_picture and check if user is logged in and created Cookie Manager

---
+++ src/main/java/org/example/kitsurecs/session/LogoutServlet.java
@@ -0,0 +1,25 @@
+//Revision History:
+//      NAME            DATE                        COMMENTS
+// Derrick Mangari  2025/04/15      Added logout servlet (deletes user session token)
+
+package org.example.kitsurecs.session;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        response.sendRedirect("login.jsp");
+    }
+}
---
+++ src/main/java/org/example/kitsurecs/session/SessionManager.java
@@ -0,0 +1,39 @@
+//Revision History:
+//      NAME            DATE                        COMMENTS
+// Derrick Mangari      2025/04/15      Created Session Manager to get Users, to check if user is logged in, get username, get profile picture
+
+package org.example.kitsurecs.session;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+public class SessionManager {
+    public static boolean isLoggedIn(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        return session != null && session.getAttribute("userId") != null;
+    }
+
+    public static String getUserId(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            return (String) session.getAttribute("userId");
+        }
+        return null;
+    }
+
+    public static String getUserName(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            return (String) session.getAttribute("username");
+        }
+        return null;
+    }
+
+    public static String getProfilePicture(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            return (String) session.getAttribute("profilePicture");
+        }
+        return null;
+    }
+}
